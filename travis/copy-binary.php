66d473fbf4b11a8718a2e9eb930910196ccead91
Clemens Sahs <clemens.sahs@slides-worker.org>
2014-09-17 23:53:53 +0200

    fix error

--- travis/copy-binary.php
+++ travis/copy-binary.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$extDirFormat = ini_get('extension_dir');
-$sourceDirFormat = __DIR__ .'/../modules/';
+$extDirFormat = ini_get('extension_dir') . '/%s';
+$sourceDirFormat = realpath(__DIR__ . '/../modules/%s');
 
 $fileName = 'xdebug.so';
 
 var_dump(sprintf($sourceDirFormat, $fileName),
-     sprintf($extDirFormat, $fileName));
+         sprintf($extDirFormat, $fileName));
